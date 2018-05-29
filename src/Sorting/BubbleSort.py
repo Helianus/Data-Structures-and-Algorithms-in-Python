2dd3fabcadf8ccf799f1e13a73bfacde77f4598a
Helianus <xinyuhelianushu@gmail.com>
2018-05-29 15:18:50 -0500

    submit comments

--- src/Sorting/BubbleSort.py
+++ src/Sorting/BubbleSort.py
@@ -2,10 +2,13 @@
 # Time Complexity: O(n^2)
 # Space Complexity: O(1)
 
+# Comparing adjacent elements by two for-loops continually
+# Then move the bigger one to its back
+
 def bubble_Sort(lst):
-    for i in range(len(lst)):
-        print(lst)
-        for j in range(1, len(lst) - i):
-            if lst[j] < lst[j - 1]:
-                lst[j - 1], lst[j] = lst[j], lst[j - 1]
+    for i in range(len(lst)): # first read
+        print(lst) # print the sorting processes
+        for j in range(1, len(lst) - i): # second read
+            if lst[j] < lst[j - 1]: # compare first read and second read
+                lst[j], lst[j - 1] = lst[j - 1], lst[j] # swap
     return lst
