28fe00086c650fac96ca153e44a586a3d4c2f073
Helianus <xinyuhelianushu@gmail.com>
2018-05-29 16:23:57 -0500

    create selection sort

---
+++ src/Sorting/SelectionSort.py
@@ -0,0 +1,15 @@
+# Selection Sort
+# Time Complexity: O(n^2)
+# Space Complexity: O(1)
+
+# Finding the minimal element continuously
+# Move the minimal element to front
+def selection_Sort(lst):
+    for i in range(len(lst)):                               # fist read
+        print(lst)                                          # print the sorting processes
+        minIndex = i                                        # set the minimal index to be the i
+        for j in range(i + 1, len(lst)):                    # second read between the sorted elements and the end of the list
+            if lst[j] < lst[minIndex]:                      # compare second read element and minimal index element
+                minIndex = j                                # set minimal index to be second read
+        lst[minIndex], lst[i] = lst[i], lst[minIndex]       # swap
+    return lst
