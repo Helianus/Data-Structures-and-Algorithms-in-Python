2577c50bdfb630e2722fd3339ef5f9e76f35e627
Helianus <xinyuhelianushu@gmail.com>
2018-05-29 16:23:41 -0500

    submit new comments

--- src/Sorting/BubbleSort.py
+++ src/Sorting/BubbleSort.py
@@ -8,7 +8,7 @@
 def bubble_Sort(lst):
     for i in range(len(lst)):                                  # first read
         print(lst)                                             # print the sorting processes
-        for j in range(1, len(lst) - i):                       # second read
+        for j in range(1, len(lst) - i):                       # second read between the first element and the length of the list - first read
             if lst[j] < lst[j - 1]:                            # compare first read and second read
                 lst[j], lst[j - 1] = lst[j - 1], lst[j]        # swap
     return lst
