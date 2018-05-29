f2f5aa8aab419cb7185499340fd5e8f693871329
Helianus <xinyuhelianushu@gmail.com>
2018-05-29 16:33:16 -0500

    fix bug

--- src/Sorting/SelectionSort.py
+++ src/Sorting/SelectionSort.py
@@ -5,11 +5,11 @@
 # Finding the minimal element continuously
 # Move the minimal element to front
 def selection_Sort(lst):
-    for i in range(len(lst)):                                       # fist read
-        print(lst)                                                  # print the sorting processes
-        minIndex = i                                                # set the minimal index to be the first read
-        for j in range(i + 1, len(lst)):                            # second read between the sorted elements and the end of the list
-            if lst[j] < lst[minIndex]:                              # compare second read element and minimal index element
-                minIndex = j                                        # set minimal index to be second read
-                lst[minIndex], lst[i] = lst[i], lst[minIndex]       # swap
+    for i in range(len(lst)):                               # fist read
+        print(lst)                                          # print the sorting processes
+        minIndex = i                                        # set the minimal index to be the first read
+        for j in range(i + 1, len(lst)):                    # second read between the sorted elements and the end of the list
+            if lst[j] < lst[minIndex]:                      # compare second read element and minimal index element
+                minIndex = j                                # set minimal index to be second read
+        lst[minIndex], lst[i] = lst[i], lst[minIndex]       # swap
     return lst
