792e995059641de239c82d5bc64f9202dd510136
Helianus <xinyuhelianushu@gmail.com>
2018-06-06 22:17:36 -0500

    add comments

--- src/Sorting/QuickSort.py
+++ src/Sorting/QuickSort.py
@@ -2,27 +2,28 @@
 # Time Complexity: O(n^2)
 # Space Complexity: O(log(n))
 
+# In-place quick sort does not create subsequences
+# its subsequence of the input is represented by a leftmost and rightmost index
 def quick_Sort(lst, first, last):
     print(lst)
-    if first >= last:
+    if first >= last:                               # the lst is sorted
         return
-    pivot = lst[last]
-    lo = first
-    hi = last - 1
+    pivot = lst[last]                               # last element of range is pivot
+    lo = first                                      # scan from first to last
+    hi = last - 1                                   # scan from last to first: "-1" is because of pivot
     while lo <= hi:
-        while lo <= hi and lst[lo] < pivot:
+        while lo <= hi and lst[lo] < pivot:         # scan until reaching value >= pivot
             lo += 1
-        while lo <= hi and pivot < lst[hi]:
+        while lo <= hi and pivot < lst[hi]:         # scan until reaching value <= pivot
             hi -= 1
-        if lo <= hi:
-            lst[lo], lst[hi] = lst[hi], lst[lo]
-            lo, hi = lo + 1, hi - 1
+        if lo <= hi:                                # check if scans did cross
+            lst[lo], lst[hi] = lst[hi], lst[lo]     # swap
+            # shrink range
+            lo += 1
+            hi -= 1                                 
 
-    lst[lo], lst[last] = lst[last], lst[lo]
+    lst[lo], lst[last] = lst[last], lst[lo]         # re-setting pivot
     quick_Sort(lst, first, lo - 1)
     quick_Sort(lst, lo + 1, last)
 
     return lst
-
-lst = [4, 0, 3, 2, 1, 5, 8, 9, 7, 6]
-print(quick_Sort(lst, 0, len(lst) - 1))
